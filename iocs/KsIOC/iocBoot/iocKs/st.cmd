b343425e681a7847df053656c047d2b52602df5d
andrewgomella <andrewgomella@gmail.com>
2015-08-04 18:15:38 -0400

    update st.cmd to work with areadetector2-2 +

--- iocs/KsIOC/iocBoot/iocKs/st.cmd
+++ iocs/KsIOC/iocBoot/iocKs/st.cmd
@@ -9,6 +9,8 @@ epicsEnvSet("QSIZE",  "200")
 epicsEnvSet("XSIZE",  "4908")
 epicsEnvSet("YSIZE",  "3264")
 epicsEnvSet("NCHANS", "2048")
+epicsEnvSet("CBUFFS", "500")
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 
 #KsCamConfig: PORT, CameraIndexNumber, maxBuffers, maxMemory, priority, stackSize
@@ -23,14 +25,14 @@ epicsEnvSet("NCHANS", "2048")
 KsCamConfig("$(PORT)", 0, 200, 0, 0 , 0 )
 
 dbLoadRecords("$(ADCORE)/db/ADBase.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADCORE)/Db/NDFile.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADCORE)/db/NDFile.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 # Note that Ks.template must be loaded after NDFile.template to replace the file format correctly
 dbLoadRecords("$(AREA_DETECTOR)/ADNikonKs/db/Ks.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin
 NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, 0)
-dbLoadRecords("$(ADCORE)/Db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
-dbLoadRecords("$(ADCORE)/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=16019712")
+#dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT)TYPE=Int16,FTVL=SHORT,NELEMENTS=16019712")
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
